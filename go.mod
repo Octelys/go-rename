a346943a55105df807b58c7c3d611bf8142eeb0c
kzryzstof <kzryzstof@proton.me>
2025-11-24 15:49:03 -0500

    Add .gitignore to exclude IDE configuration files

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module go-rename
 
 go 1.25
+
+require (
+	github.com/openai/openai-go/v3 v3.8.1 // indirect
+	github.com/tidwall/gjson v1.14.4 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.1 // indirect
+	github.com/tidwall/sjson v1.2.5 // indirect
+)
--- main.go
+++ main.go
@@ -1,21 +1,91 @@
 package main
 
 import (
-  "fmt"
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/openai/openai-go/v3"
+	"github.com/openai/openai-go/v3/option"
 )
 
 //TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
 // the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
 
 func main() {
-  //TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
-  // to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
-  s := "gopher"
-  fmt.Printf("Hello and welcome, %s!\n", s)
 
-  for i := 1; i <= 5; i++ {
-	//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-	// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
-	fmt.Println("i =", 100/i)
-  }
+	openAiApiKey := os.Getenv("OPENAI_API_KEY")
+
+	if openAiApiKey == "" {
+		fmt.Println("OPENAI_API_KEY environment variable is not set")
+		os.Exit(10000)
+	}
+
+	workingDir := os.Getenv("WORKING_DIR")
+
+	if workingDir == "" {
+		fmt.Println("WORKING_DIR environment variable is not set")
+		os.Exit(10001)
+	}
+
+	/* 1. Reads all the file names in the directory */
+
+	files, err := os.ReadDir(workingDir)
+
+	if err != nil {
+		fmt.Printf("Unable to read all the files from the working directory: %s\n", err)
+		os.Exit(10002)
+	}
+
+	fmt.Printf("The following files have been found in the working directory:\n")
+
+	for _, file := range files {
+		fmt.Printf("\tFile name: %s\n", file.Name())
+	}
+
+	/* 2. Asks the LLM to get the file format used extract the page number from the file name */
+
+	fmt.Printf("\n")
+	fmt.Printf("Requesting the assistant to guess the orders of the files... ")
+
+	//	Builds the prompt for the assistant
+	var assistantPrompt = strings.Builder{}
+	assistantPrompt.WriteString("Below are the files found in the directory. Based on the information found there, sort them according their page number in a JSON array:\n")
+
+	for _, file := range files {
+		assistantPrompt.WriteString(file.Name() + "\n")
+	}
+
+	//	Initializes the assistant
+	client := openai.NewClient(
+		option.WithAPIKey(openAiApiKey),
+	)
+
+	//	Call the assistant
+	chatCompletion, err := client.Chat.Completions.New(context.TODO(), openai.ChatCompletionNewParams{
+		Messages: []openai.ChatCompletionMessageParamUnion{
+			openai.UserMessage(assistantPrompt.String()),
+		},
+		Model: openai.ChatModelGPT5Mini,
+	})
+
+	if err != nil {
+		fmt.Printf("[ FAILED ]\n")
+		os.Exit(10003)
+	}
+
+	var orderedFiles []string
+	err = json.Unmarshal([]byte(chatCompletion.Choices[0].Message.Content), &orderedFiles)
+
+	if err != nil {
+		fmt.Printf("[ FAILED ]\n")
+		os.Exit(10004)
+	}
+
+	fmt.Printf("[ OK ]\n")
+
+	/* 3. Extract the publication month & year of the first page, assuming it the cover */
+
 }
